a3eaae76ea7ef5600864e15af8ea16f70725f3b1
f440 <freq440@gmail.com>
2013-02-08 20:32:23 +0900

    Add nuspec file and install script

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'Haskell Platform'
+$installerType = 'EXE'
+$url = 'http://lambda.haskell.org/platform/download/2012.4.0.0/HaskellPlatform-2012.4.0.0-setup.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
