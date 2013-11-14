b1c22a6ecdfbe08f9449c23a7c70b9412953f598
f440 <freq440@gmail.com>
2013-11-14 10:40:23 +0900

    Bump up version number to 2013.02

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'Haskell Platform'
 $installerType = 'EXE'
-$url = 'http://lambda.haskell.org/platform/download/2012.4.0.0/HaskellPlatform-2012.4.0.0-setup.exe'
+$url = 'http://lambda.haskell.org/platform/download/2013.2.0.0/HaskellPlatform-2013.2.0.0-setup.exe'
 $url64 = $url
 $silentArgs = '/S'
 $validExitCodes = @(0)
