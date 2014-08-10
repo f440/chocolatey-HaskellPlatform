02286d757770c4cf430bfb50879eddd0cd28847f
f440 <freq440@gmail.com>
2014-08-10 14:23:45 +0900

    Bump up version number to 2014.02

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'Haskell Platform'
 $installerType = 'EXE'
-$url = 'http://www.haskell.org/platform/download/2013.2.0.0/HaskellPlatform-2013.2.0.0-setup.exe'
-$url64 = $url
+$url = 'http://www.haskell.org/platform/download/2014.2.0.0/HaskellPlatform-2014.2.0.0-i386-setup.exe'
+$url64 = 'http://www.haskell.org/platform/download/2014.2.0.0/HaskellPlatform-2014.2.0.0-x86_64-setup.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
