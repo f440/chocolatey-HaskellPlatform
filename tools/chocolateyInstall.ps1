5f4d6b0012853d1ef063925ca4ce698456282939
f440 <freq440@gmail.com>
2015-08-01 15:55:39 +0900

    Bump up version number to 7.10.2.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,24 @@
 ﻿$packageName = 'Haskell Platform'
 $installerType = 'EXE'
-$url = 'http://www.haskell.org/platform/download/2014.2.0.0/HaskellPlatform-2014.2.0.0-i386-setup.exe'
-$url64 = 'http://www.haskell.org/platform/download/2014.2.0.0/HaskellPlatform-2014.2.0.0-x86_64-setup.exe'
+$url = 'https://www.haskell.org/platform/download/7.10.2/HaskellPlatform-7.10.2-i386-setup.exe'
+$url64 = 'https://www.haskell.org/platform/download/7.10.2/HaskellPlatform-7.10.2-x86_64-setup.exe'
 $silentArgs = '/S'
-$validExitCodes = @(0)
+$validExitCodes = @(0, 2)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+
+  checksum      = '16c2474e11305aab0361a18cd5f7a22f'
+  checksumType  = 'md5'
+  checksum64    = '7497c6e977879f0e145765a8afbc603a'
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
